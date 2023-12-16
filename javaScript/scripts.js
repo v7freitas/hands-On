ea8957fa9437c09b58ae9e798629d470e453810a
Nelio Frazao <neliofrazac@gmail.com>
2023-12-16 15:42:08 -0300

    adiciona exemplo

--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -6,124 +6,125 @@
 // studentModal.showModal() // funciona para abrir o dialog
 // studentModal.close() funciona para fechar o dialog
 
-const studentModal = document.querySelector('#student-modal');
-const studentForm = document.querySelector('#student-form');
-const studentModalTitle = document.querySelector('#student-modal-title')
-const saveStudentButton = document.querySelector('#save-student')
+const baseUrl = "http://localhost:3000";
 
-/**
- * Função responsável abrir o modal de estudante
- */
+// Passo 1: Selecionar os elementos HTML necessários
+const studentModal = document.querySelector("#student-modal");
+const studentTable = document.querySelector("#student-table");
+const studentForm = document.querySelector("#student-form");
+const studentModalTitle = document.querySelector("#student-modal-title");
+const saveStudentButton = document.querySelector("#save-student");
+// studentModal.showModal()
+// Passo 2: Definir função para abrir o modal do estudante
 const openStudentModal = () => studentModal.showModal();
-
-/**
- * Função responsável fechar o modal de estudante
- */
+const createStudent = () => {
+  studentModalTitle.innerHTML = `Novo Aluno`;
+  saveStudentButton.innerHTML = "Criar";
+  openStudentModal();
+  saveStundentData(`${baseUrl}/alunos`, "POST");
+};
+// Passo 3: Definir função para fechar o modal do estudante
 const closeStudentModal = () => studentModal.close();
-
-/**
- * Função responsável por criar linhas na tabela student-table
- * @param {nome} string
- * @param {matricula} string
- * @param {curso} string
- * @param {id} string
- */
-const createStudentTableRow = (nome, matricula, curso, id) => {
-  const studentTable = document.querySelector('#student-table tbody')
-  const tableTr = document.createElement('tr');
-  tableTr.innerHTML = ` 
-  <td>${nome}</td>
-  <td>${matricula}</td>
-  <td>${curso}</td>
-  <td align="center">
-    <button class="button button--danger" onclick=deleteStudentTable(${id})>Apagar</button>
-    <button class="button button--success" onclick="editdStudentModal(${id})">Editar</button>
-  </td>`;
+// Passo 4: Criar uma linha na tabela do estudante
+const createStudentTableRow = (id, name, matricula, curso) => {
+  const tableTr = document.createElement("tr");
+  tableTr.innerHTML = `
+    <td>${name}</td>
+    <td>${matricula}</td>
+    <td>${curso}</td>
+    <td align="center">
+      <button class="button button--danger" onclick="deleteStudentTable(${id})">Apagar</button>
+      <button class="button button--success" onclick="editdStudentModal(${id})"}>Editar</button>
+    </td>`;
   studentTable.appendChild(tableTr);
-}
+};
 
-/**
- * Função responsável savar os dados de um estudante
- * @param {url} string
- * @param {method} string
- */
 const saveStundentData = (url, method) => {
-  studentForm.addEventListener('submit', (event)=> {
+  studentForm.addEventListener("submit", (event) => {
     event.preventDefault();
+    // capturar os dados do formulário
     const formData = new FormData(studentForm);
+    // transformar os dados do formulário em um objeto
     const payload = new URLSearchParams(formData);
     fetch(url, {
-        method: method,
-        body: payload
-    })
-    .catch(error => {
-        closeStudentModal();
-        alert('ocorreu um erro tente mais tarde')
-        console.error(error);
-    })
-    // const inputs = document.querySelectorAll('input') // pega todos os iputs
-    // console.log(inputs[0].value) // acessa o primeiro indice do array de inputs
-  });
-}
-
-/**
- * Função responsável abrir o modal de aluno e salvar um novo aluno
- * @param {studentId} string
- */
-const createStudent = () => {
-  openStudentModal();
-  studentModalTitle.textContent = 'Novo Aluno';
-  saveStudentButton.textContent = 'Criar';
-  saveStundentData('http://localhost:3000/alunos',  'POST');
-}
-
-/**
- * Função responsável abrir o modal de edição e carregar os dados de um estudante e salvar os dados da edição
- * @param {studentId} string
- */
- const editdStudentModal = async (studentId)  => {
-  const url = `http://localhost:3000/alunos/${studentId}`;
-  openStudentModal();
-  studentModalTitle.textContent='Editar aluno';
-  saveStudentButton.textContent = 'Editar';
-  const [name, matricula] = document.querySelectorAll('input');
-  const selectCurso =  document.querySelector("#curso");
-  fetch(url)
-  .then(resp => resp.json())
-  .then(data => {
-    name.value = data.nome
-    matricula.value = data.matricula
-    selectCurso.value =  data.curso
-  })
-  saveStundentData(url,  'PUT');
- };
-
-/**
- * Função responsável por apagar dados de um estutande
- * @param {studentId} string
- */
-const deleteStudentTable = async (studentId)  =>  
-  fetch(`http://localhost:3000/alunos/${studentId}`, {method : 'DELETE'});
-
-/**
- * Função responsável por carregar os dados da student-table
- */
-const loadStudentTable = () => {
-  fetch('http://localhost:3000/alunos')
-  .then(resp => resp.json())
-  .then(data => {
-    data.forEach(item => {
-      createStudentTableRow(item.nome, item.matricula, item.curso, item.id)
-    })
-  }).catch((error) => {
-    alert('ocorreu um erro tente mais tarde')
-    console.error(error);
+      method: method,
+      body: payload,
+    }).catch((error) => {
+      closeStudentModal();
+      alert("Ocorreu um erro. Tente mais tarde.");
+      console.error(error);
+    });
   });
 };
 
+// Passo 7: Abrir o modal para criar um novo aluno
+// Passo 8: Excluir um aluno da tabela
+const deleteStudentTable = (id) => {
+  fetch(`${baseUrl}/alunos/${id}`, {
+    method: "DELETE",
+  }).catch((error) => {
+    alert("Ocorreu um erro. Tente mais tarde.");
+    console.error(error);
+  });
+};
+// Passo 9: Abrir o modal de edição e carregar os dados do aluno
+const editdStudentModal = (id) => {
+  fetch(`${baseUrl}/alunos/${id}`)
+    .then((resp) => resp.json())
+    .then((data) => {
+      const { nome, matricula, curso } = data;
+      studentModalTitle.innerHTML = `Editar Aluno ${nome}`;
+      document.querySelector("#nome").value = nome;
+      document.querySelector("#matricula").value = matricula;
+      document.querySelector("#curso").value = curso;
+      saveStudentButton.innerHTML = "Salvar";
+      openStudentModal();
+      saveStundentData(`${baseUrl}/alunos/${id}`, "PUT");
+    })
+    .catch((error) => {
+      alert("Ocorreu um erro. Tente mais tarde.");
+      console.error(error);
+    });
+};
+
+// Passo 10: Chamar a função para carregar dados iniciais da tabela ao carregar a página
+const loadStudentTable = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/alunos");
+    const data = await response.json();
+    data.forEach((student) => {
+      createStudentTableRow(
+        student.id,
+        student.nome,
+        student.matricula,
+        student.curso
+      );
+    });
+  } catch (error) {
+    alert("Ocorreu um erro. Tente mais tarde.");
+    console.error(error);
+  }
+};
+
+const loadStudentTable2 = () => {
+  fetch("http://localhost:3000/alunos")
+    .then((resp) => resp.json())
+    .then((data) => {
+      data.forEach((student) => {
+        // pode ser feito assim também
+        // const { nome, matricula, curso } = student;
+        createStudentTableRow(
+          student.id,
+          student.nome,
+          student.matricula,
+          student.curso
+        );
+      });
+    })
+    .catch((error) => {
+      alert("Ocorreu um erro. Tente mais tarde.");
+      console.error(error);
+    });
+};
+
 loadStudentTable();
-
-
-
-
-
